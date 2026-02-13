cdd0cce6b894e9816e80e1f59283c6e74b2ac98f
younes-nb <younesnikbin@gmail.com>
2026-02-13 19:08:38 +0330

    Fix image reference in deployment script for Custom Pod Autoscaler

--- deploy_all_cpas.sh
+++ deploy_all_cpas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NAMESPACE="online-boutique"
-IMAGE="docker.io/younesnb/predictive-k8s-autoscaling:v1.0.0"
+IMAGE="docker.io/younesnb/predictive-k8s-autoscaler:v1.0.0"
 PROMETHEUS_URL="http://prometheus-stack-kube-prom-prometheus.monitoring.svc.cluster.local:9090"
 
 for DEPLOYMENT in $(kubectl get deployments -n $NAMESPACE -o jsonpath='{.items[*].metadata.name}'); do
