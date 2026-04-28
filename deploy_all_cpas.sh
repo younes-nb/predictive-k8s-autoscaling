b91d595c52fe96c7e45c00da8b0616e607b708b8
younes-nb <younesnikbin@gmail.com>
2026-04-28 19:50:00 +0330

    Update FEATURE_SET in deploy_all_cpas.sh to include traffic metrics

--- deploy_all_cpas.sh
+++ deploy_all_cpas.sh
@@ -3,7 +3,7 @@
 NAMESPACE="online-boutique"
 IMAGE="docker.io/younesnb/predictive-k8s-autoscaler:v1.0.0"
 PROMETHEUS_URL="http://prometheus-stack-kube-prom-prometheus.monitoring.svc.cluster.local:9090"
-FEATURE_SET="cpu_mem"
+FEATURE_SET="cpu_mem_traffic"
 
 for DEPLOYMENT in $(kubectl get deployments -n $NAMESPACE -o jsonpath='{.items[*].metadata.name}'); do
     
