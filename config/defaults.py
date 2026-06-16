79513df2d53f4cec3dd2932392b8a1ded7202247
younes-nb <younesnikbin@gmail.com>
2026-06-16 18:36:22 +0330

    Export table_to_raw_columns from config/defaults.py

--- config/defaults.py
+++ config/defaults.py
@@ -1,7 +1,7 @@
 from shared.config_paths import PATHS, DATASET_TABLES, DEFAULT_CHECKPOINT_PATH, Paths
 from shared.config_preprocessing_defaults import PREPROCESSING, PreprocessingDefaults
 from shared.config_training_defaults import TRAINING, TrainingDefaults
-from shared.features import FEATURE_SETS, tables_for_feature_set
+from shared.features import FEATURE_SETS, tables_for_feature_set, table_to_raw_columns
 
 __all__ = [
     "PATHS",
@@ -11,6 +11,7 @@ __all__ = [
     "DATASET_TABLES",
     "FEATURE_SETS",
     "tables_for_feature_set",
+    "table_to_raw_columns",
     "Paths",
     "PreprocessingDefaults",
     "TrainingDefaults",
