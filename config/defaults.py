97ff86184fb643947232571b1f21f82d7d2f1de3
younes-nb <younesnikbin@gmail.com>
2026-06-16 13:57:40 +0330

    Add missing FEATURE_SETS and tables_for_feature_set to config/defaults.py

--- config/defaults.py
+++ config/defaults.py
@@ -1,6 +1,7 @@
 from shared.config_paths import PATHS, DATASET_TABLES, DEFAULT_CHECKPOINT_PATH, Paths
 from shared.config_preprocessing_defaults import PREPROCESSING, PreprocessingDefaults
 from shared.config_training_defaults import TRAINING, TrainingDefaults
+from shared.features import FEATURE_SETS, tables_for_feature_set
 
 __all__ = [
     "PATHS",
@@ -8,6 +9,8 @@ __all__ = [
     "TRAINING",
     "DEFAULT_CHECKPOINT_PATH",
     "DATASET_TABLES",
+    "FEATURE_SETS",
+    "tables_for_feature_set",
     "Paths",
     "PreprocessingDefaults",
     "TrainingDefaults",
