fcec5b692792b8d96b49ee87c600417fcd8fb97d
younes02 <younes02@node.ml-d7525.k8sautoscaledl-pg0.wisc.cloudlab.us>
2026-07-14 09:53:23 -0500

    expand TCN to 5-layer config, add statsmodels dep

--- experiments/tsdp/tcn_bigru/config.py
+++ experiments/tsdp/tcn_bigru/config.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class TcnBiGruConfig:
     TCN_KERNEL_SIZE: int = 3
-    TCN_FILTERS: tuple = (256, 256)
-    TCN_DILATIONS: tuple = (1, 2, 4)
+    TCN_FILTERS: tuple = (64, 64, 128, 128, 256)
+    TCN_DILATIONS: tuple = (1, 2, 4, 8, 16)
     TCN_DROPOUT: float = 0.2
     BIGRU_HIDDEN: tuple = (64, 128)
 
