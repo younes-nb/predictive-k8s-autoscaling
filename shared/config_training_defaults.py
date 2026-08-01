87f63b88f9fd5bcc8f4489c5b2ebffc0d21f37c9
younes02 <younes02@node.ml-d8545.k8sautoscaledl-pg0.wisc.cloudlab.us>
2026-08-01 13:13:09 -0500

    lower default training batch size to 4096

--- shared/config_training_defaults.py
+++ shared/config_training_defaults.py
@@ -7,7 +7,7 @@ class TrainingDefaults:
     HIDDEN_SIZE: int = 64
     NUM_LAYERS: int = 3
     DROPOUT: float = 0.5
-    BATCH_SIZE: int = 8192
+    BATCH_SIZE: int = 4096
     NUM_WORKERS: int = 12
     EPOCHS: int = 1000
     LR: float = 0.0005
